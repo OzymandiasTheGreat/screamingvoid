811e0268c17dcaa93e108bd3d997dcdd09e728d3
Tomas Ravinskas <tomas.rav@gmail.com>
2022-04-15 06:34:36 +0300

    Add expo

--- apps/app/android/app/src/main/java/me/screamingvoid/app/MainActivity.java
+++ apps/app/android/app/src/main/java/me/screamingvoid/app/MainActivity.java
@@ -1,4 +1,6 @@
 package me.screamingvoid.app;
+import expo.modules.ReactActivityDelegateWrapper;
+import com.facebook.react.ReactActivityDelegate;
 
 import com.facebook.react.ReactActivity;
 
@@ -11,5 +13,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Main";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegateWrapper(this,
+      new ReactActivityDelegate(this, getMainComponentName())
+    );
   }
 }
--- apps/app/index.js
+++ apps/app/index.js
@@ -1,9 +1,4 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import {registerRootComponent} from 'expo';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+registerRootComponent(App);
