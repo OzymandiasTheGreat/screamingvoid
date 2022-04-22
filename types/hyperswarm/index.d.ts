3c1a8dc2c730078452f78706576ab429fb5104b6
Tomas Ravinskas <tomas.rav@gmail.com>
2022-04-22 18:57:10 +0300

    Update hyperswarm types

--- types/hyperswarm/index.d.ts
+++ types/hyperswarm/index.d.ts
@@ -1,11 +1,11 @@
 declare module "hyperswarm" {
 	import type { Duplex } from "stream";
 
-	type KeyPair = { publicKey: Buffer, secretKey: Buffer };
+	type KeyPair = { publicKey: Buffer; secretKey: Buffer };
 
 	class PeerDiscovery {
 		flushed(): Promise<void>;
-		refresh(opts: { client: boolean, server: boolean }): Promise<void>;
+		refresh(opts: { client: boolean; server: boolean }): Promise<void>;
 		destroy(): Promise<void>;
 	}
 
@@ -19,28 +19,35 @@ declare module "hyperswarm" {
 
 	class Hyperswarm {
 		constructor(opts?: {
-			keyPair?: KeyPair,
-			seed?: Buffer,
-			maxPeers?: number,
-			firewall?: (remotePublicKey: Buffer) => boolean,
-			dht?: any,   // TODO ???
+			keyPair?: KeyPair;
+			seed?: Buffer;
+			maxPeers?: number;
+			firewall?: (remotePublicKey: Buffer) => boolean;
+			dht?: any; // TODO ???
 		});
 
-		connections: Set<any>;   // TODO Double check type
+		keyPair: KeyPair;
+		connections: Set<any>; // TODO Double check type
 		peers: Map<string, PeerInfo>;
-		dht: any;   // TODO ???
+		dht: any; // TODO ???
 		maxPeers: number;
 
-		join(topic: Buffer, opts: { client: boolean, server: boolean }): PeerDiscovery;
+		join(
+			topic: Buffer,
+			opts: { client: boolean; server: boolean },
+		): PeerDiscovery;
 		leave(topic: Buffer): Promise<void>;
-		joinPeer(noisePublicKey: Buffer): void;   // TODO Double check return type
+		joinPeer(noisePublicKey: Buffer): void; // TODO Double check return type
 		leavePeer(noisePublicKey: Buffer): void;
 		status(topic: Buffer): PeerDiscovery;
 		listen(): Promise<void>;
 		flush(): Promise<void>;
 		destroy(): Promise<void>;
 
-		on(event: "connection", cb: (socket: Duplex, info: PeerInfo) => void): void;
+		on(
+			event: "connection",
+			cb: (socket: Duplex, info: PeerInfo) => void,
+		): void;
 	}
 
 	export default Hyperswarm;
