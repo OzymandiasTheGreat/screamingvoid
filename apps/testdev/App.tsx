ecc28d66adbc4dd8d96a10bc9eb082ce3df08876
Tomas Ravinskas <tomas.rav@gmail.com>
2022-07-18 20:19:48 +0300

    Add test app Add new JSI module shim

---
+++ apps/testdev/App.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Sodium } from "@screamingvoid/fs-next";
+
+export default function App() {
+	useEffect(() => {
+		console.log(Sodium.test);
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
