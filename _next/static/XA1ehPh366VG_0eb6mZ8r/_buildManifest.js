432428939dfd713da2c84403dc5c2b8d750d63a6
OzymandiasTheGreat <OzymandiasTheGreat@users.noreply.github.com>
2022-08-07 09:57:32 +0000

    deploy: d340b1abf4a803971670726bc5e2a8bf5e8f8271

---
+++ _next/static/XA1ehPh366VG_0eb6mZ8r/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/a45b9c2f-54e87da25a871989.js","static/chunks/979-158266a44d9cab30.js","static/chunks/pages/index-ea52d6703c0c56cd.js"],"/_error":["static/chunks/pages/_error-464b78ed7544b06b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
